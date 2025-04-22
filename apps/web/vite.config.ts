45d48f81477b8164d97e54dd7c190c509248af06
maomao <meladryyy@gmail.com>
2025-04-22 19:59:37 +0800

    [chore] initial web project setup

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    }
+  }
+})
